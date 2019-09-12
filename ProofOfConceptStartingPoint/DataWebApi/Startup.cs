c58ff9e01d307068f04ee06458a546d48c08afa5
Diaban Kouame <diabankouame@gmail.com>
2019-09-12 16:40:52 -0500

    Adding Configuring Swagger1

--- ProofOfConceptStartingPoint/DataWebApi/Startup.cs
+++ ProofOfConceptStartingPoint/DataWebApi/Startup.cs
@@ -26,18 +26,26 @@ namespace DataWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "DataWebApi", Version = "v1" });
-              
             });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -50,7 +58,8 @@ namespace DataWebApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
-            app.UseSwagger();
+
+
         }
     }
 }
